2b8a6afcb3de6c8b1da6a59b3b4fde17f23a90a2
Maksim <homeice@mail.ru>
2022-11-13 04:55:19 +0300

    Решение задачи

---
+++ Step_4/Program.cs
@@ -0,0 +1,22 @@
+﻿string[] array = new string[5];
+string[] SmallArray = new string[0];
+for (int i = 0; i < array.Length; i++)
+{
+    Console.WriteLine($"Введите значение {i+1}: ");
+    array[i] = Console.ReadLine();
+}
+
+Console.Write("[" + string.Join(", ", array) + "] -> ");
+
+int size = 3;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i].Length <= size)
+    {
+        Array.Resize(ref SmallArray, SmallArray.Length + 1);
+        SmallArray[SmallArray.Length-1] = array[i];
+    }
+}
+Console.Write("[" + string.Join(", ", SmallArray) + "]");
+
+
